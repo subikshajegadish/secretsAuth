7798a4ca2ab841da9cdc729ee4110947ae2e479f
Subiksha Jegadish <subiksha.jegadish@gmail.com>
2023-06-27 16:36:00 +0530

    SessionPassport-V1

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -16,14 +18,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}))
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,29 +46,12 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password:req.body.password
-  });
-  newUser.save().then(function(doc){
-    res.render("secrets");
-  }).catch(function(err){
-    console.log(err);
-  })
+
 });
 
 app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({email: username}).then(function(foundUser){
-    if(foundUser){
-      if(foundUser.password === password){
-        res.render("secrets");
-      }
-    }
-  })
-})
+});
 
 
 
